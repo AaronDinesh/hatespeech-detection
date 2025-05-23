f1084aec264e4e605f700ea72805372a7f815588
Aaron Dinesh <aarondinesh2002@gmail.com>
2025-05-23 16:14:11 +0200

    idek.. at this point I want to jump off a building

--- llm_detection/LlaVA_finetuning.py
+++ llm_detection/LlaVA_finetuning.py
@@ -317,7 +317,7 @@ class LlavaModelPLModule(L.LightningModule):
             torch.optim.Optimizer: The optimizer for training.
         """
         # you could also add a learning rate scheduler if you want
-        optimizer = Adam8bit(self.parameters(), lr=self.config.get("lr"))
+        optimizer = Adam8bit(self.model.get_peft_parameters(), lr=self.config.get("lr"))
         #optimizer = DeepSpeedCPUAdam(self.parameters(), lr=self.config.get("lr"))
 
         return optimizer
